5c6cf215607ef05955b1393a6d1fec83a85d67be
andreww <andrew.wilson@linn.co.uk>
2013-01-21 14:56:40 +0000

    Distribute argument transformers to their own files.

---
+++ src/ManagedApiBuilder/ArgumentTransformers/VoidReturnTransformer.cs
@@ -0,0 +1,19 @@
+﻿using ApiParser;
+
+namespace ManagedApiBuilder.ArgumentTransformers
+{
+    class VoidReturnTransformer : IArgumentTransformer
+    {
+        public bool Apply(IFunctionSpecificationAnalyser aNativeFunction, IFunctionAssembler aFunctionAssembler)
+        {
+            if (aNativeFunction.CurrentParameter != null) { return false; }
+            if (!aNativeFunction.ReturnType.MatchToPattern(new NamedCType("void")).IsMatch)
+            {
+                return false;
+            }
+            aFunctionAssembler.SetManagedReturn(new CSharpType("void"));
+            aNativeFunction.ConsumeReturn();
+            return true;
+        }
+    }
+}
